2979ef8dfd3849d7e5bf78f5925441e80a08473c
HJavid010 <HJavid0010@gmail.com>
2024-06-09 14:35:41 +0330

    added clients and games handling for Server

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -1,8 +1,26 @@
 package server;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Random;
 
 public class Server {
+    ArrayList<Client> clients = new ArrayList<>();
+    HashMap<String, Game> games = new HashMap<>();
+
+    void createGame(Client client) {
+        Game newGame = new Game(tokenGenerator());
+        games.put(newGame.token, newGame);
+        newGame.joinClient(client);
+    }
+
+    void joinGame(Client client, String token) {
+        Game game;
+        if ((game = games.get(token)) != null) {
+            game.joinClient(client);
+        }
+    }
+
     public static String tokenGenerator() {
         StringBuilder token = new StringBuilder();
         Random random = new Random();
