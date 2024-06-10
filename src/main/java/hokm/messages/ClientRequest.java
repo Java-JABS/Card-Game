05b4d40e39a33f75c6e3435a0d0cbdbbc5447130
daniel <d.boveiry@yahoo.com>
2024-06-10 17:33:29 +0330

    completed ClientRequest

--- src/main/java/hokm/messages/ClientRequest.java
+++ src/main/java/hokm/messages/ClientRequest.java
@@ -3,4 +3,16 @@ package hokm.messages;
 public class ClientRequest {
     protected String token;
     protected ClientRequestType type;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public ClientRequestType getType() {
+        return type;
+    }
 }
