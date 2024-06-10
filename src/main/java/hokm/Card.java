739766b9dd164428f7c24d64d61dd296984b8480
HJavid010 <HJavid0010@gmail.com>
2024-06-10 19:16:47 +0330

    Added new features to Dast and moved new dast to its class

--- src/main/java/hokm/Card.java
+++ src/main/java/hokm/Card.java
@@ -1,7 +1,5 @@
 package hokm;
 
-import java.util.Collections;
-
 public class Card {
     public final CardsSuit cardSuit;
     public final CardValues card;
@@ -9,17 +7,6 @@ public class Card {
     public Card(CardValues card, CardsSuit cardSuit) {
         this.card = card;
         this.cardSuit = cardSuit;
-    }
-
-    public static Dast getCards() {
-        Dast result = new Dast();
-        for (CardsSuit suit : CardsSuit.values()) {
-            for (CardValues cardValues : CardValues.values()) {
-                result.add(new Card(cardValues, suit));
-            }
-        }
-        Collections.shuffle(result);
-        return result;
     }
 }
 
--- src/main/java/hokm/Dast.java
+++ src/main/java/hokm/Dast.java
@@ -1,6 +1,7 @@
 package hokm;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Dast extends ArrayList<Card> {
     boolean contains(CardsSuit suit) {
@@ -14,5 +15,18 @@ public class Dast extends ArrayList<Card> {
         Card card = get(index);
         remove(index);
         return card;
+    }
+
+    public Dast() {
+
+    }
+
+    public Dast(boolean shuffled) {
+        for (CardsSuit suit : CardsSuit.values()) {
+            for (CardValues cardValues : CardValues.values()) {
+                add(new Card(cardValues, suit));
+            }
+        }
+        if (shuffled) Collections.shuffle(this);
     }
 }
