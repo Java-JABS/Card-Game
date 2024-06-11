c13934cc29c07e134fbc187e6e13145b74230908
daniel <d.boveiry@yahoo.com>
2024-06-11 14:43:48 +0330

    created createUser and getToken in db

--- src/main/java/hokm/server/Database.java
+++ src/main/java/hokm/server/Database.java
@@ -11,17 +11,35 @@ public class Database {
         connection = DriverManager.getConnection("jdbc:sqlite:" + filename);
     }
 
+
     void initialize() {
+
+        try {
+            Statement statement = connection.createStatement();
+            statement.execute("create table users (id INT PRIMARY KEY, username varchar(255) UNIQUE NOT NULL,token varchar(64) NOT NULL)");
+        } catch (SQLException ignored) {
+        }
     }
 
     String getToken(String username) {
-        try (PreparedStatement preparedStatement = connection.prepareStatement("SELECT token FROM Users WHEN username = ?")) {
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement("SELECT token FROM users WHERE username = ?")) {
             preparedStatement.setString(1, username);
             ResultSet resultSet = preparedStatement.executeQuery();
             return (resultSet.next()) ? resultSet.getString(1) : null;
 
         } catch (SQLException e) {
+            return null;
+        }
+    }
+    void createUser(String username ,String token){
+        try(PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO users (username, token) VALUES (?, ?);")) {
+            preparedStatement.setString(1,username);
+            preparedStatement.setString(2,token);
+            ResultSet resultSet = preparedStatement.executeQuery();
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
+
 }
