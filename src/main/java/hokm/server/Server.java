3991a8c1f425c9e7bb7101ed4abe89227a1c0f91
HJavid010 <HJavid0010@gmail.com>
2024-06-11 13:47:04 +0330

    rewrote Server class for multithreading handling clients

--- src/main/java/hokm/server/Server.java
+++ src/main/java/hokm/server/Server.java
@@ -1,12 +1,107 @@
 package hokm.server;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import hokm.messages.ClientRequest;
+import hokm.messages.ClientRequestType;
+import hokm.messages.JoinRequest;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Random;
 
-public class Server {
-    ArrayList<Client> clients = new ArrayList<>();
-    HashMap<String, Game> games = new HashMap<>();
+public class Server extends Thread {
+    private static final ArrayList<Socket> clientRequests = new ArrayList<>();
+    private static final HashMap<String, Game> games = new HashMap<>();
+    private static ServerSocket serverSocket;
+    private static final GsonBuilder builder = new GsonBuilder();
+    private static final Gson gsonAgent = builder.create();
+    private DataOutputStream sendBuffer;
+    private DataInputStream receiveBuffer;
+    @Override
+    public void run() {
+        Socket socket;
+        while (true) {
+            synchronized (clientRequests) {
+                while (clientRequests.isEmpty()) {
+                    try {
+                        clientRequests.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                socket = clientRequests.get(0);
+                clientRequests.remove(0);
+            }
+            if (socket != null) {
+                //Todo handleConnection(socket);
+            }
+        }
+    }
+
+    private void handleConnection(Socket socket) {
+        String clientRequestJson;
+
+        try {
+            receiveBuffer = new DataInputStream(
+                    new BufferedInputStream(socket.getInputStream())
+            );
+            sendBuffer = new DataOutputStream(
+                    new BufferedOutputStream(socket.getOutputStream())
+            );
+
+
+            clientRequestJson = receiveBuffer.readUTF();
+            ClientRequest msg = (ClientRequest) gsonAgent
+                    .fromJson(clientRequestJson, gsonAgent.fromJson(clientRequestJson, ClientRequest.class).getType().className);
+            switch (msg.getType()){
+                case JOIN:
+                    //Todo
+                    break;
+                case GAME_CREATE:
+                    //Todo
+                    break;
+            }
+
+            sendBuffer.close();
+            receiveBuffer.close();
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void listen() throws IOException {
+        Socket socket;
+        while (true) {
+            socket = serverSocket.accept();
+            synchronized (clientRequests) {
+                clientRequests.add(socket);
+                clientRequests.notify();
+            }
+        }
+    }
+
+    private Server(ServerSocket serverSocket, int workerNumbs) throws IOException {
+        Server.serverSocket = serverSocket;
+        for (int i = 0; i < workerNumbs; i++) {
+            new Server().start();
+        }
+        listen();
+    }
+
+    private Server() {
+    }
+
+    static void runInstance(int port) {
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            new Server(serverSocket, 10);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     void createGame(Client client) {
         Game newGame = new Game(tokenGenerator());
