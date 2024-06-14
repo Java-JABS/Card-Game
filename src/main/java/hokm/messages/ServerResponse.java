dbc8bf412dcb4b499e06200cd8c44af2bf980f59
HJavid010 <HJavid0010@gmail.com>
2024-06-14 12:17:57 +0330

    ServerResponse class got completed

--- src/main/java/hokm/messages/ServerResponse.java
+++ src/main/java/hokm/messages/ServerResponse.java
@@ -1,4 +1,19 @@
 package hokm.messages;
 
 public class ServerResponse {
+    private final boolean success;
+    private final String additionalInfo;
+
+    public ServerResponse(boolean success, String info) {
+        this.success = success;
+        this.additionalInfo = info;
+    }
+
+    public boolean wasSuccessful() {
+        return this.success;
+    }
+
+    public String getAdditionalInfo() {
+        return this.additionalInfo;
+    }
 }
