8a7fdd9cd7041a0a2454a9cfc0284703657b55cf
Amir <salehamir708@gmail.com>
2024-06-26 15:46:32 +0330

    up_player panel is added but not completed

--- src/main/java/hokm/client/masoud_gui/Up_player.java
+++ src/main/java/hokm/client/masoud_gui/Up_player.java
@@ -1,2 +1,25 @@
-package hokm.client.masoud_gui;public class Up_player {
+package hokm.client.masoud_gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Up_player extends JPanel {
+
+    private int width;
+    private int height;
+
+    public Up_player(int _width, int _height){
+        JLabel pic = new JLabel();
+        super.setLayout(new BorderLayout());
+        pic.setSize(_width/2, _height/2);
+        add(pic, BorderLayout.CENTER);
+        pic.setIcon(new ImageIcon(new ImageIcon("pictures/Person/person.png").getImage().getScaledInstance(_width/2,-1, Image.SCALE_SMOOTH)));
+        JLabel text = new JLabel("Player");
+        text.setSize(50, 30);
+        add(text, BorderLayout.SOUTH);
+        pic.setVisible(true);
+        text.setVisible(true);
+
+    }
+
 }
