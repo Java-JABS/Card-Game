4f042321e90b1a999a0b7e74dc8218006c982fd0
Kourosh-Abbasi <strangeforever83@gmail.com>
2024-06-27 13:58:41 +0330

    Completed ClientRequestSender class

--- src/main/java/hokm/client/ClientRequestSender.java
+++ src/main/java/hokm/client/ClientRequestSender.java
@@ -13,48 +13,30 @@ import java.net.Socket;
 public class ClientRequestSender {
     final String hostAddress;
     final int port;
-    private final GsonBuilder builder = new GsonBuilder();
-    private final Gson gsonAgent = builder.create();
+    private transient final GsonBuilder builder = new GsonBuilder();
+    private transient final Gson gsonAgent = builder.create();
+    private String token;
 
     public ClientRequestSender(String hostAddress, int port) {
         this.hostAddress = hostAddress;
         this.port = port;
     }
 
-    ServerResponse sendMessage(String message) {
-        try (Socket socket = new Socket(hostAddress, port);) {
+    <T extends ClientRequest> String sendMessage(T message) throws RequestException {
+        try (Socket socket = new Socket(hostAddress, port)) {
             DataOutputStream sendBuffer = new DataOutputStream(socket.getOutputStream());
             DataInputStream receiveBuffer = new DataInputStream(socket.getInputStream());
-            sendBuffer.writeUTF(message);
-            return gsonAgent.fromJson(receiveBuffer.readUTF(), ServerResponse.class);
+            message.setToken(token);
+            sendBuffer.writeUTF(gsonAgent.toJson(message));
+            ServerResponse response = gsonAgent.fromJson(receiveBuffer.readUTF(), ServerResponse.class);
+            if(!response.wasSuccessful()) throw new RequestException(response.getAdditionalInfo());
+            return response.getAdditionalInfo();
         } catch (IOException e) {
             throw new RuntimeException();
         }
     }
 
-    String register(String username) throws RequestException {
-        RegisterRequest request = new RegisterRequest();
-        request.setUsername(username);
-        String message = gsonAgent.toJson(request);
-        ServerResponse response = sendMessage(message);
-        if(!response.wasSuccessful()) throw new RequestException(response.getAdditionalInfo());
-        return response.getAdditionalInfo();
+    public void setToken(String token) {
+        this.token = token;
     }
-    void join(String token) throws RequestException {
-        throw new RequestException("");
-    }
-    void room_create(){
-
-    }
-
-//    REGISTER(RegisterRequest.class),
-//    LOGIN(LoginRequest .class),
-//    JOIN(JoinRequest .class),
-//    ROOM_CREATE(RoomCreateRequest .class),
-//    LEAVE(LeaveRequest.class),
-//    GAME_START(GameStartRequest.class),
-//    GAME_UPDATE(GameUpdateRequest.class),
-//    ROOM_UPDATE(RoomUpdateRequest.class),
-//            ;
-
 }
