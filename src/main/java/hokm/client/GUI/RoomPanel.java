d4c9584695ef33d38fa97277b55ee8648a849b67
Kourosh-Abbasi <strangeforever83@gmail.com>
2024-06-28 14:15:36 +0330

    Room panel improved.

--- src/main/java/hokm/client/GUI/RoomPanel.java
+++ src/main/java/hokm/client/GUI/RoomPanel.java
@@ -2,6 +2,8 @@ package hokm.client.GUI;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import static java.lang.Thread.sleep;
 
@@ -10,9 +12,47 @@ public class RoomPanel extends JPanel {
     JButton startButton = new JButton("Start");
 
     public RoomPanel(){
-        setLayout(new FlowLayout());
-        add(startButton);
-        add(names);
+        startButton.setPreferredSize(new Dimension(300,100));
+        startButton.setFont(new Font("Arial", Font.BOLD, 25));
+        GridBagConstraints startButtonGrid = new GridBagConstraints();
+        startButtonGrid.gridx = 0;
+        startButtonGrid.gridy = 0;
+        startButtonGrid.insets = new Insets(5,5,5,5);
+        startButton.setFocusable(false);
+        startButton.addActionListener(actionEvent -> {
+            JFrame topFrame = (JFrame) SwingUtilities.getWindowAncestor(this);
+            topFrame.remove(this);
+            topFrame.add(new OuterGamePanel());
+            topFrame.revalidate();
+            topFrame.repaint();
+        });
+        startButton.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent mouseEvent) {}
+            @Override
+            public void mousePressed(MouseEvent mouseEvent) {}
+            @Override
+            public void mouseReleased(MouseEvent mouseEvent) {}
+            @Override
+            public void mouseEntered(MouseEvent mouseEvent) {
+                startButton.setBackground(Color.green);
+            }
+            @Override
+            public void mouseExited(MouseEvent mouseEvent) {
+                startButton.setBackground(new JButton().getBackground());
+            }
+        });
+
+        names.setPreferredSize(new Dimension(300,100));
+        names.setFont(new Font("Arial", Font.BOLD, 25));
+        GridBagConstraints namesGrid = new GridBagConstraints();
+        namesGrid.gridx = 0;
+        namesGrid.gridy = 1;
+        namesGrid.insets = new Insets(5,5,5,5);
+
+        setLayout(new GridBagLayout());
+        add(startButton, startButtonGrid);
+        add(names, namesGrid);
         new Thread(()->{
             Integer a=0;
             while (true){
@@ -24,12 +64,6 @@ public class RoomPanel extends JPanel {
                 }
             }
         }).start();
-        startButton.addActionListener(actionEvent -> {
-            JFrame topFrame = (JFrame) SwingUtilities.getWindowAncestor(this);
-            topFrame.remove(this);
-            topFrame.add(new OuterGamePanel());
-            topFrame.revalidate();
-            topFrame.repaint();
-        });
+
     }
 }
